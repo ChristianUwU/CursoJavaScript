67a2ed18158ab53a27e9d67d47be27937de2ecea
Christian <obama060296@gmail.com>
2023-07-15 22:09:35 -0400

    arreglos

---
+++ 01_Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,24 @@
+
+
+    // const arr =  new Array(10);
+    // const arr = [];
+    // console.log(arr)
+
+    let videoJuegos = ['Mario 3', 'Megaman', 'Chrono', 'Trigger'];
+    console.log( videoJuegos[0] );
+
+    const arregloCosas = [
+        true,
+        123,
+        'Christian',
+        1 + 2,
+        function(){},
+        ()=>{},
+        { a: 1},
+        ['X', 'Megaman', 'Zero', 'Dr pool', [
+            'Dr wylly',
+            'Woodman'
+        ]]
+    ]
+    // console.log( {arregloCosas} );
+    console.log(arregloCosas[7][4][1]);
